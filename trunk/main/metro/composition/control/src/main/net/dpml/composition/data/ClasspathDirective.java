70e0fcc9f9931440bd899071fa7821bdcc0f3c14
mcconnell <mcconnell@00579e91-1ffa-0310-aa18-b241b61564ef>
2005-07-12 14:27:32 +0000

    Remove usage of FilesetDirectives as these can be handled via uris.

--- trunk/main/metro/composition/control/src/main/net/dpml/composition/data/ClasspathDirective.java
+++ trunk/main/metro/composition/control/src/main/net/dpml/composition/data/ClasspathDirective.java
@@ -29,13 +29,7 @@ import java.net.URI;
  */
 public final class ClasspathDirective implements Serializable
 {
-     private static final FilesetDirective[] EMPTY_FILESETS = new FilesetDirective[0]; 
      private static final URI[] EMPTY_URIS = new URI[0]; 
-
-    /**
-     * The fileset directives
-     */
-    private FilesetDirective[] m_filesets;
 
     /**
      * The resource references
@@ -62,30 +56,7 @@ public final class ClasspathDirective implements Serializable
      */
     public ClasspathDirective( final String name, final URI[] uris )
     {
-        this( name, null, uris );
-    }
-
-    /**
-     * Create a ClasspathDirective instance.
-     *
-     * @param filesets the filesets to be included in a classloader
-     * @param uris the set of artifact uris
-     */
-    public ClasspathDirective( 
-       final String name, 
-       final FilesetDirective[] filesets, 
-       final URI[] uris )
-    {
         m_name = name;
-        if( filesets == null )
-        {
-            m_filesets = EMPTY_FILESETS;
-        }
-        else
-        {
-            m_filesets = filesets;
-        }
-
         if( uris == null )
         {
             m_uris = EMPTY_URIS;
@@ -105,14 +76,13 @@ public final class ClasspathDirective implements Serializable
         return m_name;
     }
 
-
    /**
     * Return the default status of this directive.  If TRUE
     * the enclosed repository and fileset directives are empty.
     */
     public boolean isEmpty()
     {
-        final int n = m_uris.length + m_filesets.length;
+        final int n = m_uris.length;
         return n == 0;
     }
 
@@ -124,16 +94,6 @@ public final class ClasspathDirective implements Serializable
     public URI[] getURIs()
     {
         return m_uris;
-    }
-
-    /**
-     * Return the set of fileset directives.
-     *
-     * @return the fileset directives
-     */
-    public FilesetDirective[] getFilesets()
-    {
-        return m_filesets;
     }
 
     public boolean equals( Object other )
@@ -168,26 +128,8 @@ public final class ClasspathDirective implements Serializable
                             return false;
                         }
                     }
+                    return true;
                 }
-                if( getFilesets().length != directive.getFilesets().length )
-                {
-                    return false;
-                }
-                else
-                {
-                    FilesetDirective[] mine = getFilesets();
-                    FilesetDirective[] yours = directive.getFilesets();
-                    for( int i=0; i<mine.length; i++ )
-                    {
-                        FilesetDirective d = mine[i];
-                        FilesetDirective e = yours[i];
-                        if( false == d.equals( e ) )
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
             }
             else
             {
@@ -203,10 +145,6 @@ public final class ClasspathDirective implements Serializable
     public int hashCode()
     {
         int hash = m_name.hashCode();
-        for( int i=0; i<m_filesets.length; i++ )
-        {
-            hash ^= m_filesets[i].hashCode();
-        }
         for( int i=0; i<m_uris.length; i++ )
         {
             hash ^= m_uris[i].hashCode();
