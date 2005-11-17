4d4afee98c62371de32917ad0ff9d5ecb0cdcadd
mcconnell <mcconnell@00579e91-1ffa-0310-aa18-b241b61564ef>
2005-11-17 13:09:14 +0000

    Copied remotely

---
+++ trunk/main/depot/station/api/src/main/net/dpml/station/info/RegistryDescriptor.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2005 Stephen J. McConnell.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dpml.station.info;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Hashtable;
+import java.beans.SimpleBeanInfo;
+import java.beans.BeanDescriptor;
+import java.beans.DefaultPersistenceDelegate;
+
+/**
+ * The RegistryDescriptor is immutable datastructure used to 
+ * hold the state of an application registry.
+ *
+ * @author <a href="@PUBLISHER-URL@">@PUBLISHER-NAME@</a>
+ * @version @PROJECT-VERSION@
+ */
+public class RegistryDescriptor extends AbstractDescriptor
+{
+    public static final String DEFAULT_STORAGE_PATH = "local:xml:dpml/metro/registry";
+    
+    public static final URI DEFAULT_STORAGE_URI = createDefaultStorageURI();
+    
+    private final Entry[] m_entries;
+    
+   /**
+    * Creation of a new registry descriptor.
+    * @param entries an array of application entries
+    */
+    public RegistryDescriptor( Entry[] entries )
+    {   
+        m_entries = entries;
+    }
+    
+   /**
+    * Returns the array of application descriptors.
+    * 
+    * @return the application entry array
+    */
+    public Entry[] getEntries()
+    {
+        return m_entries;
+    }
+    
+    private static URI createDefaultStorageURI()
+    {
+        try
+        {
+            return new URI( DEFAULT_STORAGE_PATH );
+        }
+        catch( Exception e )
+        {
+            return null; // will not happen
+        }
+    }
+    
+    public static final class Entry
+    {
+        final String m_key;
+        final ApplicationDescriptor m_descriptor;
+        
+        public Entry( String key, ApplicationDescriptor descriptor )
+        {
+            m_key = key;
+            m_descriptor = descriptor;
+        }
+        
+        public String getKey()
+        {
+            return m_key;
+        }
+        
+        public ApplicationDescriptor getApplicationDescriptor()
+        {
+            return m_descriptor;
+        }
+    }
+
+   /**
+    * Entry bean info.
+    */
+    public static final class EntryBeanInfo extends SimpleBeanInfo
+    {
+        private static final BeanDescriptor BEAN_DESCRIPTOR = setupBeanDescriptor();
+        
+       /**
+        * Return the bean descriptor.
+        * @return the descriptor
+        */
+        public BeanDescriptor getBeanDescriptor()
+        {
+            return BEAN_DESCRIPTOR;
+        }
+        
+        private static BeanDescriptor setupBeanDescriptor()
+        {
+            BeanDescriptor descriptor = new BeanDescriptor( Entry.class );
+            descriptor.setValue( 
+              "persistenceDelegate", 
+              new DefaultPersistenceDelegate( 
+                new String[]{"key", "applicationDescriptor"} ) );
+            return descriptor;
+        }
+    }
+}
---
+++ trunk/main/depot/station/api/src/main/net/dpml/station/info/StartupPolicy.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2005 Stephen J. McConnell.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dpml.station.info;
+
+import net.dpml.transit.util.Enum;
+
+/**
+ * Lifestyle policy enumeration.
+ * @author <a href="@PUBLISHER-URL@">@PUBLISHER-NAME@</a>
+ * @version @PROJECT-VERSION@
+ */
+public final class StartupPolicy extends Enum
+{
+    static final long serialVersionUID = 1L;
+
+   /**
+    * Disable policy.
+    */
+    public static final StartupPolicy DISABLED = new StartupPolicy( "disabled" );
+
+   /**
+    * Manual startup policy.
+    */
+    public static final StartupPolicy MANUAL = new StartupPolicy( "manual" );
+
+   /**
+    * Automatic startup policy.
+    */
+    public static final StartupPolicy AUTOMATIC = new StartupPolicy( "automatic" );
+
+   /**
+    * Array of scope enumeration values.
+    */
+    private static final StartupPolicy[] ENUM_VALUES = new StartupPolicy[]{DISABLED, MANUAL, AUTOMATIC};
+
+   /**
+    * Returns an array of activation enum values.
+    * @return the activation policies array
+    */
+    public static StartupPolicy[] values()
+    {
+        return ENUM_VALUES;
+    }
+    
+   /**
+    * Internal constructor.
+    * @param label the enumeration label.
+    */
+    private StartupPolicy( String label )
+    {
+        super( label );
+    }
+    
+   /**
+    * Return a string representation of the scope.
+    * @return the string value
+    */
+    public String toString()
+    {
+        return getName().toUpperCase();
+    }
+    
+   /**
+    * Return a startup policy value matching the supplied value.
+    * @param value the policy name
+    * @return the policy
+    * @exception IllegalArgumentException if the name if not recognized
+    */
+    public static StartupPolicy parse( String value ) throws IllegalArgumentException
+    {
+        if( value.equalsIgnoreCase( "disabled" ) )
+        {
+            return DISABLED;
+        }
+        else if( value.equalsIgnoreCase( "manual" ) )
+        {
+            return MANUAL;
+        }
+        else if( value.equalsIgnoreCase( "automatic" ) )
+        {
+            return AUTOMATIC;
+        }
+        else
+        {
+            final String error =
+              "Unrecognized scope argument [" + value + "]";
+            throw new IllegalArgumentException( error );
+        }
+    }
+}
+
