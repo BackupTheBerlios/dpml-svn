318a418859c93a53b68e169eb9736e7ef45d52fd
mcconnell <mcconnell@00579e91-1ffa-0310-aa18-b241b61564ef>
2005-12-05 15:55:34 +0000

    add configuration component test case

---
+++ trunk/main/metro/test/src/test/net/dpml/metro/runtime/test/ConfigTestCase.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2005 Stephen J. McConnell.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dpml.metro.runtime.test;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.awt.Color;
+import java.io.File;
+import java.net.URI;
+import java.lang.reflect.Proxy;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import junit.framework.TestCase;
+
+import net.dpml.metro.model.ComponentModel;
+import net.dpml.metro.model.ContextModel;
+import net.dpml.metro.part.Part;
+import net.dpml.metro.part.Directive;
+import net.dpml.metro.part.Controller;
+import net.dpml.metro.part.ActivationPolicy;
+import net.dpml.metro.part.Instance;
+import net.dpml.metro.part.Component;
+import net.dpml.metro.part.ControlException;
+import net.dpml.metro.part.Model;
+import net.dpml.metro.state.State;
+import net.dpml.metro.state.StateListener;
+import net.dpml.metro.state.StateEvent;
+import net.dpml.metro.state.impl.DefaultStateListener;
+
+import net.dpml.transit.model.UnknownKeyException;
+import net.dpml.transit.Value;
+
+import net.dpml.test.config.ConfigurableComponent;
+
+/**
+ * Contains a series of tests dealing with dynamic component lifecycles.
+ *
+ * @author <a href="@PUBLISHER-URL@">@PUBLISHER-NAME@</a>
+ * @version @PROJECT-VERSION@
+ */
+public class ConfigTestCase extends TestCase
+{    
+    private Model m_model;
+    
+    public void setUp() throws Exception
+    {
+        final String path = "config.part";
+        final File test = new File( System.getProperty( "project.test.dir" ) );
+        final URI uri = new File( test, path ).toURI();
+        m_model = Part.CONTROLLER.createModel( uri );
+    }
+    
+   /**
+    * Test that the component initial state is inactive.
+    */
+    public void testCategories() throws Exception
+    {
+        Component component = Part.CONTROLLER.createComponent( m_model );
+        assertNotNull( "component", component );
+        Instance instance = component.getInstance();
+        ConfigurableComponent object = (ConfigurableComponent) instance.getValue( false );
+        assertEquals( "name", "fred", object.getName() );
+        assertEquals( "count", 3, object.getItemCount() );
+        component.deactivate();
+    }
+    
+    static
+    {
+        System.setProperty( 
+          "java.util.logging.config.class", 
+          System.getProperty( 
+            "java.util.logging.config.class", 
+            "net.dpml.transit.util.ConfigurationHandler" ) );
+    }
+}
