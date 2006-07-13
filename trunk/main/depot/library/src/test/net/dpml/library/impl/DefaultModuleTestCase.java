006e8fd10bf0df758fe3f5fe566ffc6019da025d
mcconnell <mcconnell@00579e91-1ffa-0310-aa18-b241b61564ef>
2006-07-13 06:54:30 +0000

    housekeeping

--- trunk/main/depot/library/src/test/net/dpml/library/impl/DefaultModuleTestCase.java
+++ trunk/main/depot/library/src/test/net/dpml/library/impl/DefaultModuleTestCase.java
@@ -122,10 +122,10 @@ public class DefaultModuleTestCase extends AbstractTestCase
         try
         {
             String path = "dpml";
-            //doProviderTest( path, false, 8, 4, 0 );
-            //doProviderTest( path, true, 8, 39, 0 );
-            doProviderTest( path, false );
-            doProviderTest( path, true );
+            doProviderTest( path, false, 0, 4, 0 );
+            doProviderTest( path, true, 0, 34, 0 );
+            //doProviderTest( path, false );
+            //doProviderTest( path, true );
         }
         catch( Exception e )
         {
@@ -134,10 +134,10 @@ public class DefaultModuleTestCase extends AbstractTestCase
         try
         {
             String path = "dpml/transit";
-            //doProviderTest( path, false, 2, 2, 0 );
-            //doProviderTest( path, true, 2, 4, 0 );
-            doProviderTest( path, false );
-            doProviderTest( path, true );
+            doProviderTest( path, false, 2, 2, 0 );
+            doProviderTest( path, true, 2, 4, 0 );
+            //doProviderTest( path, false );
+            //doProviderTest( path, true );
         }
         catch( Exception e )
         {
